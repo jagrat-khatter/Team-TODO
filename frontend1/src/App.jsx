10aa43bfe4c5324788203517ee2856d3c8852da9
jagrat-khatter <jagratkhatter@gmail.com>
2025-04-19 12:43:48 +0000

    Added the navigate to signup part

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route ,Navigate } from 'react-router-dom';
 import './App.css';
 import SignupPage from './components/signupPage'; // Keep the correct PascalCase import
 import Dashboard from './components/Dashboard'; // Assuming you have a Dashboard component
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         {/* You can add more routes here for other pages */}
